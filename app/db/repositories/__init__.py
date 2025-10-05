eae12e00bf4ba5661c964a95f45fd1a1089eb274
anton-zelenskiy <antonfewwt@gmail.com>
2025-10-05 22:16:56 +0700

    bet repo

--- app/db/repositories/__init__.py
+++ app/db/repositories/__init__.py
@@ -3,6 +3,7 @@ Repository layer for database operations using SQLAlchemy
 """
 
 from .base_repository import BaseRepository
+from .betting_opportunity_repository import BettingOpportunityRepository
 from .league_repository import LeagueRepository
 from .match_repository import MatchRepository
 from .team_repository import TeamRepository
@@ -14,5 +15,6 @@ __all__ = [
     'TelegramUserRepository',
     'MatchRepository',
     'LeagueRepository',
+    'BettingOpportunityRepository',
     'TeamRepository',
 ]
